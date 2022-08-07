c18d5a4b170f23bdeaa05e8f9c8e0121cbf0bd74
Anna Yurchenko <anichkayu@yandex.by>
2022-08-07 19:43:20 +0300

    init(hw-2): init of project

---
+++ src/components/CountryItem.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const CountryItem = () => {
+  return <div>CountryItem</div>;
+};
---
+++ src/components/CountryList.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const CountryList = () => {
+  return <div>CountryList</div>;
+};
